e697b772e6299c74c77de1d1b0f11420de2aba2b
Erick Brower <erick@johnhancockdigital.com>
2017-09-27 14:50:23 -0400

    Added seed data, fixed default column value in answers, added CORS

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170927011651) do
+ActiveRecord::Schema.define(version: 20170927184210) do
 
   create_table "answers", force: :cascade do |t|
     t.string "text"
-    t.boolean "correct"
+    t.boolean "correct", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "question_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create(email: 'cerickbrower@gmail.com')
+
+exam = Exam.create(title: "Awesome test exam", user: user)
+
+question1 = Question.create(text: "What is your favorite color?", exam: exam)
+question2 = Question.create(text: "What is the square root of 4?", exam: exam)
+question3 = Question.create(text: "Who is the president of the US?", exam: exam)
+
+answer11 = Answer.create(text: "blue", question: question1)
+answer12 = Answer.create(text: "green", question: question1)
+answer13 = Answer.create(text: "black", question: question1, correct: true)
+answer14 = Answer.create(text: "red", question: question1)
+
+answer21 = Answer.create(text: "1", question: question2)
+answer22 = Answer.create(text: "5", question: question2)
+answer23 = Answer.create(text: "4", question: question2)
+answer24 = Answer.create(text: "2", question: question2, correct: true)
+
+answer31 = Answer.create(text: "Tyler Durden", question: question3)
+answer32 = Answer.create(text: "Hillary Clinton", question: question3)
+answer33 = Answer.create(text: "Tom Bombadil", question: question3)
+answer34 = Answer.create(text: "Donald Trump", question: question3, correct: true)
